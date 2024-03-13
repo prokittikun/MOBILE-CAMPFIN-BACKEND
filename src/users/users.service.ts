8a8a65f991f7348807e6ad49627b957b94da4383
prokittikun <pro.kittikun.2546@gmail.com>
2024-03-14 01:03:41 +0700

    add update profile route

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LogService } from '../utils/log/log.service';
 import { PrismaService } from '../database/prisma.service';
 import { Prisma, User } from '@prisma/client';
-
+import { ReqUpdateProfileDataDto } from './dto/requests/req-update-profile-data.dto';
+import { ResDataDto } from '../DTO/res-data.dto';
+import { EnumStatus } from '../enum/status.enum';
+import { Request } from 'express';
+import { s3 } from 'src/utils/S3-Client';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
 @Injectable()
 export class UsersService {
   private logger = new LogService(UsersService.name);
 
   constructor(private prisma: PrismaService) {}
 
+  async ApiUpdateProfile(
+    req: Request,
+    updateUserDto: ReqUpdateProfileDataDto,
+    profileImage: Express.Multer.File,
+  ) {
+    const tag = this.ApiUpdateProfile.name;
+    try {
+      const res: ResDataDto<User> = {
+        statusCode: EnumStatus.success,
+        data: await this.updateProfile(req, updateUserDto, profileImage),
+        message: '',
+      };
+      return res;
+    } catch (error) {
+      this.logger.error(`${tag} -> `, error);
+      throw error;
+    }
+  }
+
+  async updateProfile(
+    req: Request,
+    updateUserDto: ReqUpdateProfileDataDto,
+    profileImage: Express.Multer.File,
+  ): Promise<User> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { id: req.user.sub },
+      });
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
+      }
+      let data;
+      if (profileImage) {
+        const fileName = user.id + 'jpg';
+        const imageUrl = fileName;
+        data = {
+          ...updateUserDto,
+          profileImage: imageUrl,
+        };
+        await s3.send(
+          new PutObjectCommand({
+            Bucket: 'camp',
+            Key: fileName,
+            Body: profileImage.buffer,
+          }),
+        );
+      } else {
+        data = {
+          ...updateUserDto,
+        };
+      }
+      const userUpdate = await this.update({
+        where: { id: req.user.sub },
+        data,
+      });
+
+      return userUpdate;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   async create(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
+    const tag = this.create.name;
+    return await this.prisma.user.create({
       data,
     });
   }
@@ -21,7 +89,7 @@ export class UsersService {
     userWhereUniqueInput: Prisma.UserWhereUniqueInput,
     select?: Prisma.UserSelect,
   ): Promise<User | null> {
-    return this.prisma.user.findUnique({
+    return await this.prisma.user.findUnique({
       where: userWhereUniqueInput,
       select,
     });
@@ -31,7 +99,7 @@ export class UsersService {
     userWhereInput: Prisma.UserWhereInput,
     select?: Prisma.UserSelect,
   ): Promise<User | null> {
-    return this.prisma.user.findFirst({
+    return await this.prisma.user.findFirst({
       where: userWhereInput,
       select,
     });
@@ -46,7 +114,7 @@ export class UsersService {
     orderBy?: Prisma.UserOrderByWithRelationInput;
   }): Promise<User[]> {
     const { skip, take, cursor, where, orderBy, select } = params;
-    return this.prisma.user.findMany({
+    return await this.prisma.user.findMany({
       skip,
       take,
       cursor,
@@ -61,14 +129,14 @@ export class UsersService {
     data: Prisma.UserUpdateInput;
   }): Promise<User> {
     const { where, data } = params;
-    return this.prisma.user.update({
+    return await this.prisma.user.update({
       data,
       where,
     });
   }
 
   async delete(where: Prisma.UserWhereUniqueInput): Promise<User> {
-    return this.prisma.user.delete({
+    return await this.prisma.user.delete({
       where,
     });
   }
